ea8e93f248f7ffb1929dbed5c03a17c354d57071
chakannom <genteelson@gmail.com>
2019-07-26 21:20:03 +0900

    Generated with JHipster 6.1.2

--- src/main/java/com/chakans/portal/config/DefaultProfileUtil.java
+++ src/main/java/com/chakans/portal/config/DefaultProfileUtil.java
@@ -3,7 +3,6 @@ package com.chakans.portal.config;
 import io.github.jhipster.config.JHipsterConstants;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.core.env.Environment;
 
 import java.util.*;
 
